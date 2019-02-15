6930579bea9c3e30a165b5cddcfd43a9433e8a29
g-lefundes <gabriel.lefundes@vtex.com.br>
2019-02-15 15:19:44 -0200

    Add Collection type and fix minor issues

--- node/resolvers/catalog/index.ts
+++ node/resolvers/catalog/index.ts
@@ -140,8 +140,10 @@ export const queries = {
 
   brands: async (_, __, { dataSources: { catalog } }: Context) => catalog.brands(),
 
-  brandSearch: async (_, { query }, { dataSources: { catalog } }) =>
-    catalog.brandSearch(query) as Promise<Brand[]>,
+  brandSearch: async (_, { query }, { dataSources: { catalog } }) => {
+    const brands = (await catalog.brandSearch(query)) as Brand[]
+    return brands
+  },
 
   category: async (_, { id }, { dataSources: { catalog } }) =>
     catalog.category(id),
@@ -163,15 +165,14 @@ export const queries = {
   },
 
   collectionSearch: async (_, { query }, { dataSources: { catalog } }) => {
-    const data = await catalog.collectionSearch(query)
-
-    const collections = data.items
-
+    const collections = (await catalog.collectionSearch(query)) as Collection[]
     return collections
   },
 
-  departmentSearch: async (_, { query }, { dataSources: { catalog } }) =>
-    catalog.categorySearch(query) as Category[],
+  departmentSearch: async (_, { query }, { dataSources: { catalog } }) => {
+    const departments = (await catalog.categorySearch(query)) as Category[]
+    return departments
+  },
 
   search: async (_, args, ctx: Context) => {
     const { map: mapParams, query } = args
@@ -227,15 +228,11 @@ export const queries = {
     }
   },
 
-<<<<<<< HEAD
   searchContextFromParams: async (
     _,
     args,
     { dataSources: { catalog } }: Context
   ) => {
-=======
-  searchContextFromParams: async (_, args, { dataSources: { catalog } }) => {
->>>>>>> Prettify
     const response = {
       brand: null,
       category: null,
@@ -276,9 +273,29 @@ export const queries = {
   }
 }
 
+interface Brand {
+  id: string
+  name: string
+  isActive: boolean
+  title: string
+  metaTagDescription: string
+}
+
 interface Category {
   id: string
   name: string
   url: string
   children: Category[]
+  hasChildren: boolean
+  Title: string
+  MetaTagDescription: string
+}
+
+interface Collection {
+  id: number
+  name: string
+  searchable: boolean
+  highlight: boolean
+  dateFrom: string
+  dateTo: string
 }
